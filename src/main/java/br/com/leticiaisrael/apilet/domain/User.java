15ddf234a38afa15ea97c6bd330e1ce944a85cb4
Letícia Israel <leticiafisrael@gmail.com>
2023-01-19 12:42:56 -0300

    projeto iniciado

---
+++ src/main/java/br/com/leticiaisrael/apilet/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.leticiaisrael.apilet.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
