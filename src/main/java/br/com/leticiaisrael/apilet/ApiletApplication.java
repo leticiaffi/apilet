15e0ead824d6ed2a08be0ebd7b98b4bcbb6a3247
Letícia Israel <leticiafisrael@gmail.com>
2023-01-19 12:12:15 -0300

    projeto iniciado

---
+++ src/main/java/br/com/leticiaisrael/apilet/ApiletApplication.java
@@ -0,0 +1,13 @@
+package br.com.leticiaisrael.apilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leticiaisrael/apilet/ApiletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leticiaisrael.apilet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
